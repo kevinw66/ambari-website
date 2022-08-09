f1d5b169d5cca58414554dbe33ac4a8abb9a38fb
YeSifan <yesifan66@gmail.com>
2022-08-09 14:35:21 +0800

    change config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Ambari',
   tagline: 'The Apache Ambari project is aimed at making Hadoop management simpler by developing software for provisioning , managing, and monitoring Apache Hadoop clusters .',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://ambari.apache.org/',
+  baseUrl: '/ambari-website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/ambari.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'apache', // Usually your GitHub org/user name.
+  projectName: 'Ambari', // Usually your repo name.
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
